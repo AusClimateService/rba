5977d19e4d82ff2175c5a92402d1f346a3c8148d
Damien Irving <irving.damien@gmail.com>
2025-11-17 17:31:07 +1100

    More work on likelihoods script

--- likelihoods.py
+++ likelihoods.py
@@ -1,13 +1,94 @@
-"""Command line program for calculating likleihoods"""
+"""Command line program for calculating likelihoods"""
 
 import argparse
 
 import numpy as np
 import pandas as pd
+import cmdline_provenance as cmdprov
+
+
+def prep_data(df):
+    """Prepare data for percentile and likelihood calculations."""
+
+    ref_start = 1950
+    ref_end = 2014
+    if args.metric == 'SPEI':
+        df = df.drop(['month', 'model', 'experiment'], axis=1).groupby(['year', 'run']).mean().reset_index()
+    else:
+        df = df.drop(['model', 'experiment'], axis=1) 
+    df_ref = df[(df['year'] >= ref_start) & (df['year'] <= ref_end)]
+    df_data = df.drop(['year', 'run'], axis=1)
+    df_ref_data = df_ref.drop(['year', 'run'], axis=1)
+
+    return df_data, df_ref_data
 
 
 def main(args):
     """Run the program."""
+
+    df = pd.read_csv(args.infile)
+    df_data, df_ref_data = prep_data(df)
+    nruns = len(df['run'].unique())
+    window = nruns * 20
+    model = df['model'].unique()[0]
+    experiment = df['experiment'].unique()[-1]
+    runs = list(df['run'].unique())
+    history = cmdprov.new_log()
+    metric_label = args.metric.lower()
+    locations = 'aus-states-cities' if args.metric == 'WSDI' else 'aus-states'
+
+    if args.metric == "SPEI":
+        quantiles = np.array([0.02, 0.25, 0.33, 0.05, 0.1])
+    else:
+        quantiles = np.array([0.9, 0.95, 0.967, 0.975, 0.98])
+    thresholds = df_ref_data.quantile(quantiles)
+
+    # write out likelihoods
+    for quantile in quantiles:
+        percentile = quantile * 100
+        if quantile > 0.5:
+            df_threshold_test = df_data > threshold
+        else:
+            df_threshold_test = df_data < threshold
+        odds = df_threshold_test.rolling(window, step=nruns, center=True).apply(lambda s: (sum(s) / window) * 100)
+        odds.index = df['year'].unique()
+        odds = odds.dropna()
+        odds.index.name = 'year'
+        odds = odds.round(decimals=1)
+        start_year = odds.index[0]
+        end_year = odds.index[-1]
+
+        pre_header_lines = [
+            f"Metric: {args.metric}",
+            f"Threshold: {percentile} percentile over the 1950-2014 period",
+            f"Model: {model}",
+            f"Experiment: {experiment}",
+            f"Runs: {runs}",
+            "Description: Probability (%) of exceeding the threshold, calculated empirically across all model runs for a 20-year rolling window centered on each year"
+            f"File history: {history}"
+        ]
+        outfile = "f{args.outdir}/{metric_label}_yr_{percentile}p-likelihood_{model}_{experiment}_{locations}_{start_year}-{end_year}.csv"
+        with open(args.outfile, mode='w') as f:
+            for line in pre_header_lines:
+                f.write(line + '\n')
+            odds.to_csv(f, mode='a', header=True, index=True)
+
+    # write out thresholds
+    thresholds.index = thresholds.index * 100
+    thresholds.index.name = 'percentile'
+    pre_header_lines = [
+        f"Metric: {args.metric}",
+        f"Threshold: Percentiles over the 1950-2014 period",
+        f"Model: {model}",
+        f"Experiment: {experiment}",
+        f"Runs: {runs}",
+        f"File history: {history}"
+    ]
+    outfile = "f{args.outdir}/{metric_label}_yr_percentiles_{model}_{experiment}_{locations}_1950-2014.csv"
+    with open(args.outfile, mode='w') as f:
+        for line in pre_header_lines:
+            f.write(line + '\n')
+        odds.to_csv(f, mode='a', header=True, index=True)
 
 
 if __name__ == '__main__':
@@ -16,6 +97,7 @@ if __name__ == '__main__':
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument("infile", type=str, help="input file name")
-    parser.add_argument("outfile", type=str, help="output file name")
+    parser.add_argument("metric", type=str, choices=('WSDI', 'SPEI', 'FFDIx', 'FFDIgt90p'), help="input metric")
+    parser.add_argument("outdir", type=str, help="output directory")
     args = parser.parse_args()
     main(args)
